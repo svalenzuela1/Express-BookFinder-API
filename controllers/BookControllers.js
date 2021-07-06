a54e403412da20a4d119135de76070a92f62644b
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-07-06 03:49:11 -0400

    search query route working like regular get route

--- controllers/BookControllers.js
+++ controllers/BookControllers.js
@@ -30,6 +30,23 @@ try{
 }
 })
 
+//Get all books by Query
+router.get('/search', async (req, res) =>{
+    try{
+        //NOTE: research how to modify query alphabetically
+        //instead of by release date
+        const query = await Book.find(req.query).sort({releaseDate: -1 })
+
+        res.status(200).json({
+            statusCode: 200,
+            message:"Here are all the books that match the query",
+            query
+        })
+    }catch(error){
+        res.status(400).json({statusCode:400, error: "Query didnt work"})
+    }
+})
+
 //will create Book
 router.post("/", async (req, res) =>{
 
@@ -59,8 +76,32 @@ router.get("/search/oneBook", async(req, res) =>{
     }
 })
 
+//update the Book that is clicked on
+router.patch('/update/:id', async(req, res) =>{
+    try{
+        //find BY ID
+        //const {authors, ISBN, title, genre, releaseDate} = req.body
+        //const bookAuthors = await Book.findByIdAndUpdate({authors})
+        // const bookISBN = Book.findByIdAndUpdate({ISBN})
+        // const bookTitle = Book.findByIdAndUpdate({title})
+        // const bookGenre = Book.findByIdAndUpdate({genre})
+        // const bookReleaseDate = Book.findByIdAndUpdate({releaseDate})
+
+        const book = await Book.findOneAndUpdate({_id: req.params.id}, req.body)
+
+        res.status(200).json({
+            statusCode: 200,
+            message: "Update was Made",
+            book
+        })
+
+    }catch(error){
+        res.status(400).json({error: "Update was not made"})
+    }
+})
 
 
+//STILL WORKING ON THIS ROUTE
 //Search By Genre
 router.get('/genre', async(req, res) =>{
     try{
