9b1ba363d33ecf76958d0d50012c0c30f897bb67
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-07-11 05:18:37 -0400

    modified schema

--- db/db.js
+++ db/db.js
@@ -1,6 +1,8 @@
 //allows us to read from env file
 require("dotenv").config()
+
 const Book = require('../models/BookSchema')
+
 //mongoose manages relationship between Node.js & MongoDB
 const mongoose = require("mongoose")
 const config = {
@@ -23,46 +25,59 @@ mongoose.connection
 
 //KEEP UNCOMMENTED IN CASE I NEED TO DROP DATABASE AGAIN
 //seed
-//let addBooks = [
-    // {
-    //     "authors": ["J.D Salinger"],
-    //     "ISBN": 255,
-    //     "title": "The Catcher in the Rye",
-    //     "genre": "Fiction",
-    //     "releaseDate": "July 16, 1951",
-    // },
-    //     {
-    //     "authors": [],
-    //     "ISBN": 135,
-    //     "title": "They Both Die at the End",
-    //     "genre": "Fiction",
-    //     "releaseDate": "December 18, 2018",
-    // },
-    // {
-    //     "authors": [],
-    //     "ISBN": 223,
-    //     "title": "The Fault in Our Stars",
-    //     "genre": "Fiction",
-    //     "releaseDate": "January 10, 2012",
-    // },
-    // {
-    //     "authors": [],
-    //     "ISBN": 344,
-    //     "title": "Looking for Alaska",
-    //     "genre": "Fiction",
-    //     "releaseDate": "March 3, 2005",
-    // },
-    // {
-    //     "authors": [
-    //         "Truman Capote"
-    //     ],
-    //     "ISBN": 400,
-    //     "title": "In Cold Blood",
-    //     "genre": "True Crime",
-    //     "releaseDate": "January 17, 1966",
-    // },
-//]
+let addBooks = [
+// {
+//     "authors": ["Adam Silvera"],
+//     "ISBN": 211,
+//     "title": "They Both Die at the End",
+//     "genre": "Fiction",
+//     "releaseDate": "September 5, 2017",
+// },
+// {
+//     "authors": ["John Green"],
+//     "ISBN": 223,
+//     "title": "The Fault in Our Stars",
+//     "genre": "Fiction",
+//     "releaseDate": "January 10, 2012",
+// },
+// {
+//     "authors": ["John Green"],
+//     "ISBN": 226,
+//     "title": "Looking for Alaska",
+//     "genre": "Fiction",
+//     "releaseDate": "March 3, 2005",
+// },
+// {
+//     "authors": ["Truman Capote"],
+//     "ISBN": 400,
+//     "title": "In Cold Blood",
+//     "genre": "True Crime",
+//     "releaseDate": "January 17, 1966",
+// },
+// {
+//     "authors": ["J.D Salinger"],
+//     "ISBN": 255,
+//     "title": "The Catcher in the Rye",
+//     "genre": "Fiction",
+//     "releaseDate": "July 16, 1951",
+// },
+// {
+//     "authors": ["Ann Aguirre", "Rachel Caine"],
+//     "ISBN": 301,
+//     "title": "Honor Among Thieves",
+//     "genre": "Fiction",
+//     "releaseDate": "January 17, 2025",
+// },
+//     {
+//     "authors": ["Ernest Hemingway"],
+//     "ISBN": 200,
+//     "title": "The Sun Also Rises",
+//     "genre": "Fiction",
+//     "releaseDate": "October 22, 1926",
+// },
+]
 
+//will loop through array above and add each object to database
 // addBooks.forEach(book =>{
 //     Book.create(book)
 //
--- models/BookSchema.js
+++ models/BookSchema.js
@@ -4,10 +4,12 @@ const BookSchema = new Schema({
     ISBN: {type: Number, unique: true, required: true},
     authors: {type: Array},
     title: {type: String, required: true},
-    genre: {type: String},
+    imageURL: {type: String},
+    genre: {type: Array},
     description: {type: String},
     releaseDate: {type: String, required: true},
     rating: {type: Schema.Types.ObjectId, ref: "Rating"}
+
 })
 
 //allows user to make search queries
