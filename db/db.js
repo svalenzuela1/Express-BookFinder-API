5593d6dfd909d539450cf19c9ffe4a5cba5811d6
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-07-11 07:21:09 -0400

    uncommenting out .env file for aws deployment

--- db/db.js
+++ db/db.js
@@ -26,62 +26,62 @@ mongoose.connection
 //KEEP UNCOMMENTED IN CASE I NEED TO DROP DATABASE AGAIN
 //seed
 let addBooks = [
-// {
-//     "authors": ["Adam Silvera"],
-//     "ISBN": 211,
-//     "title": "They Both Die at the End",
-//     "genre": "Fiction",
-//     "releaseDate": "September 5, 2017",
-// },
-// {
-//     "authors": ["John Green"],
-//     "ISBN": 223,
-//     "title": "The Fault in Our Stars",
-//     "genre": "Fiction",
-//     "releaseDate": "January 10, 2012",
-// },
-// {
-//     "authors": ["John Green"],
-//     "ISBN": 226,
-//     "title": "Looking for Alaska",
-//     "genre": "Fiction",
-//     "releaseDate": "March 3, 2005",
-// },
-// {
-//     "authors": ["Truman Capote"],
-//     "ISBN": 400,
-//     "title": "In Cold Blood",
-//     "genre": "True Crime",
-//     "releaseDate": "January 17, 1966",
-// },
-// {
-//     "authors": ["J.D Salinger"],
-//     "ISBN": 255,
-//     "title": "The Catcher in the Rye",
-//     "genre": "Fiction",
-//     "releaseDate": "July 16, 1951",
-// },
-// {
-//     "authors": ["Ann Aguirre", "Rachel Caine"],
-//     "ISBN": 301,
-//     "title": "Honor Among Thieves",
-//     "genre": "Fiction",
-//     "releaseDate": "January 17, 2025",
-// },
-//     {
-//     "authors": ["Ernest Hemingway"],
-//     "ISBN": 200,
-//     "title": "The Sun Also Rises",
-//     "genre": "Fiction",
-//     "releaseDate": "October 22, 1926",
-// },
+{
+    "authors": ["Adam Silvera"],
+    "ISBN": 211,
+    "title": "They Both Die at the End",
+    "genre": "Fiction",
+    "releaseDate": "September 5, 2017",
+},
+{
+    "authors": ["John Green"],
+    "ISBN": 223,
+    "title": "The Fault in Our Stars",
+    "genre": "Fiction",
+    "releaseDate": "January 10, 2012",
+},
+{
+    "authors": ["John Green"],
+    "ISBN": 226,
+    "title": "Looking for Alaska",
+    "genre": "Fiction",
+    "releaseDate": "March 3, 2005",
+},
+{
+    "authors": ["Truman Capote"],
+    "ISBN": 400,
+    "title": "In Cold Blood",
+    "genre": "True Crime",
+    "releaseDate": "January 17, 1966",
+},
+{
+    "authors": ["J.D Salinger"],
+    "ISBN": 255,
+    "title": "The Catcher in the Rye",
+    "genre": "Fiction",
+    "releaseDate": "July 16, 1951",
+},
+{
+    "authors": ["Ann Aguirre", "Rachel Caine"],
+    "ISBN": 301,
+    "title": "Honor Among Thieves",
+    "genre": "Fiction",
+    "releaseDate": "January 17, 2025",
+},
+    {
+    "authors": ["Ernest Hemingway"],
+    "ISBN": 200,
+    "title": "The Sun Also Rises",
+    "genre": "Fiction",
+    "releaseDate": "October 22, 1926",
+},
 ]
 
 //will loop through array above and add each object to database
-// addBooks.forEach(book =>{
-//     Book.create(book)
-//
-// })
+addBooks.forEach(book =>{
+    Book.create(book)
+
+})
 
 
 module.exports = mongoose
