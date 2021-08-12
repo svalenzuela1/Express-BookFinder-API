5bf61097aa24194d713a1198abe647361f98e49f
stephaniev <stephanieemelina13@gmail.com>
2021-08-11 21:09:20 -0400

    added user model

---
+++ models/UserSchema.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose')
+
+const UserSchema = new Schema({
+    username: {type: String, required: true, unique: true, maxlength: 30, minLength: 3},
+    password: {type: String, required: true, maxlength: 50, minLength: 3}
+})
+
+const User = model('user', UserSchema)
+
+module.exports = User
+
