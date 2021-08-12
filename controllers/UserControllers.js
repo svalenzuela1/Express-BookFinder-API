c53ab3e776da81d9737f946b83d0658b005da075
stephaniev <stephanieemelina13@gmail.com>
2021-08-11 22:07:25 -0400

    user controllers

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -1,1 +1,41 @@
-require('dotenv').config()
+require('dotenv').config
+
+const User = require('../models/UserSchema')
+
+const jwt = require('jsonwebtoken')
+const router = require('express').Router()
+
+const { SECRET } = process.env
+
+//Create User
+router.post('/signup', async (req, res) =>{
+    try{
+        const newUser = await User.create(req.body)
+
+        res.status(200).json({message: 'User Successfully Created', newUser})
+
+    }catch(error){
+        res.status(400).json({error: 'Could Not Create User, Please Try Again.'})
+    }
+})
+
+//User login
+router.post('/login', async (req, res) =>{
+    try{
+        const {username, password} = req.body
+
+        const user = await User.findOne({username})
+
+        //check if user exists
+        if(user){
+            const token = await jwt.sign({username}, SECRET)
+
+            res.status(200).json({user, token})
+        } else {
+            res.status(400).json({error: 'User Does Not Exist'})
+        }
+
+    }catch(error){
+        res.status(400).json({error: 'Credentials Do Not Match. Please Try Again'})
+    }
+})
